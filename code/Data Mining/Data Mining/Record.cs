6fe53ed564fba7400a07c077bb290a1e55b1eaf5
Szarley <abdulcahir@gmail.com>
2014-01-08 16:10:59 +0100

    added Record class
    simple struct for records

---
+++ code/Data Mining/Data Mining/Record.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Data_Mining
+{
+    struct Record
+    {
+        private List<double> entity;
+
+        public List<double> Entity
+        {
+            get { return entity; }
+            // set { entity = value; }
+        }
+
+        #region /-- konstruktory --/
+        public Record(List<double> _record)
+        {
+            entity = new List<double>();
+            foreach (double elem in _record)
+                entity.Add(elem);
+        }
+
+        public Record(double[] _record)
+        {
+            entity = new List<double>();
+            for (int i = 0; i < _record.Length; i++)
+            {
+                entity.Add(_record[i]);
+            }
+        }
+        #endregion
+
+
+    }
+}
+
+
+
